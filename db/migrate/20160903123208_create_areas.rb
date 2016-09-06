a2006b7626d2fe23e806809d3cac53e093778ef5
tankos <tankos@localhost.localdomain>
2016-09-06 20:08:23 +0300

    for key del

--- db/migrate/20160903123208_create_areas.rb
+++ db/migrate/20160903123208_create_areas.rb
@@ -2,7 +2,7 @@ class CreateAreas < ActiveRecord::Migration
   def change
     create_table :areas do |t|
       t.string :name
-      t.references :parent, index: true, foreign_key: true
+      t.references :parent, index: true, foreign_key: false
 
       t.timestamps null: false
     end
